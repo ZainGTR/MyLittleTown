802dbb4e2354c8b1831f615c324c3526276883a2
Valireum <contact@valireum.net>
2023-04-01 07:33:21 +0400

    Initial idea

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+# The idea is to create a town for every user in a country where users from the same region play toguether, 
+# the town can have farms for recources, houses for workers and schools and millitary for defence/attaques. 
+# I have 16 days until the deadline, if I manage one big task per day I can make it, I never used pygames before, I work with more visual IDE like unreal, I need the money so, just deal with it.
+# Steps: 
+# 1 pygame docs and setup + init code 
+# 2 database connection and user registration and town init 
+# 3 a grid system to manage spots in the town and spawn new buildings 
+# 4 input controller and setup clicks events
+# 5 will see from there 
+
