f5f6580068545c14bec67cad3bf6f7be7cfa0265
Valireum <contact@valireum.net>
2023-04-01 07:47:05 +0400

    pygame basic init test

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import pygame
-
 
 # The idea is to create a town for every user in a country where users from the same region play toguether, 
 # the town can have farms for recources, houses for workers and schools and millitary for defence/attaques. 
@@ -11,3 +9,28 @@ import pygame
 # 4 input controller and setup clicks events
 # 5 will see from there 
 
+# 1 pygame setup
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # bg color I guess
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
